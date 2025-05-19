d3cd2884318fc6b375917088dacb4bf605859fc4
pranavmanglani <sssfacts20@gmail.com>
2025-05-19 10:37:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import os
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.header("Coolhello189's Profolio")
+st.balloons()
+st.image(os.path.join('G:\gui\static\me.png'))
+st.markdown('''Coolhello189 is a **legendary** player who plays servers like mineberry, netherstarsmp, crazymc, flamesmp,dragonsmp,etc''')
+st.markdown('''He is the king of India's Minecraft Community''')
+st.button("Click to celebrate with balloons")
